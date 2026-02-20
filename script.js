78e6527752e332b4db9a1961c5d8805e01a061e1
MohammedMqat <mohammedmqat@gmail.com>
2026-02-20 22:24:04 +0200

    inserted cell size number of times into container that in the html

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const cell
-for (let i = 0; i <= 2 && i >= 100; i++) {
-
-  
+const container = document.getElementById("container");
+console.log(container);
+const cellSize = 1;
+if (!Number.isInteger(cellSize)) {
+  throw new Error("Cellsize is not integer");
+}
+if (cellSize > 100 || cellSize < 2) {
+  throw new Error(
+    "Cellsize must be between 2 and 100, its currently: " + cellSize
+  );
+}
+for (let i = 0; i < cellSize; i++) {
+  const cellDiv = document.createElement("div");
+  cellDiv.textContent = "Cell";
+  container.appendChild(cellDiv);
 }
